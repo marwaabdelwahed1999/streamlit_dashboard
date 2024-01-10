a20f0666e4634fe5918de5ef3029dcee5f35a282
Your Name <meroabdelwahed477@gmail.com>
2024-01-10 05:00:01 +0200

    1

--- bikes_sales_dashboard.py
+++ bikes_sales_dashboard.py
@@ -1,6 +1,6 @@
 import streamlit.components.v1 as com
 import streamlit as st
-import streamlit_extras
+from streamlit_extras.metric_cards import style_metric_cards
 import plotly.express as px
 import numpy as np
 import pandas as pd
