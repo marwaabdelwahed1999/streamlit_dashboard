7531eb946b81fa34d99226866619420ad75f41bf
Your Name <meroabdelwahed477@gmail.com>
2024-01-10 12:16:45 +0200

    c

--- bikes_sales_dashboard.py
+++ bikes_sales_dashboard.py
@@ -165,7 +165,7 @@ st.markdown("""
 # nav_bar start 
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
-st.markdown("""
+st.markdown(""" 
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #e9bd01;position:fixed; top:46px;height:48px">
   <a class="navbar-brand" href="" target="_blank" style= "color: black; font-weight: bold">Bike Sales Analysis Dasboard</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
