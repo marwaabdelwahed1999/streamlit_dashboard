5ee521f6bbe1cb74ec8cf83e4b7a6b8a2c6a6c3d
Your Name <meroabdelwahed477@gmail.com>
2024-01-10 04:49:19 +0200

    2

--- bikes_sales_dashboard.py
+++ bikes_sales_dashboard.py
@@ -273,23 +273,23 @@ total_orders_count = len(bikes_selection['Date'])
 total1,total2,total3,total4,total5=st.columns(5,gap='small')
 with total1:
         st.info('Total Quantity',icon="💰")
-        st.metric(label="",value=f" {total_sales:,.0f}")
+        st.metric(label=".",value=f" {total_sales:,.0f}")
 
 with total2:
         st.info('Total Cost',icon="💰")
-        st.metric(label="",value=f"$ {total_cost:,.0f}")
+        st.metric(label=".",value=f"$ {total_cost:,.0f}")
 
 with total3:
         st.info('Total Revenue',icon="💰")
-        st.metric(label="",value=f"$ {total_revenue:,.0f}")
+        st.metric(label=".",value=f"$ {total_revenue:,.0f}")
 
 with total4:
         st.info('Total Profit',icon="💰")
-        st.metric(label="",value=f"$ {total_profit:,.0f}")
+        st.metric(label=".",value=f"$ {total_profit:,.0f}")
 
 with total5:
         st.info('Total Orders',icon="💰")
-        st.metric(label="",value=f"{total_orders_count:,.0f}")
+        st.metric(label=".",value=f"{total_orders_count:,.0f}")
 
     
 style_metric_cards(background_color="#FFFFFF",border_left_color="#686664",border_color="#000000",box_shadow="#F71938")
